9adfa76229fea2974488d2f42ede1ff9ca2b26f8
Ebrahim Zulqarni <ezulqanri2@gmail.com>
2021-01-06 15:24:09 -0600

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,18 @@ from spotipy.oauth2 import SpotifyOAuth
 
 
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+drake_uri = 'spotify:artist:3TVXtAsR1Inumwj472S9r4'
+
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="e7398f843f1a48bab3d2c197aa795d2c",
                                                client_secret="c218209ac72943a09f9c5d3f4c3193d4",
                                                redirect_uri="https://www.google.com/",
                                                scope="user-library-read",
                                                cache_path='None'))
 spotify = sp
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+results = spotify.artist_top_tracks(drake_uri)
 
-for album in albums:
-    print(album['name'])
+for track in results['tracks'][:100]:
+    print('track    : ' + track['name'])
+
+    print()
